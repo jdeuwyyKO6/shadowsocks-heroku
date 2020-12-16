f71cd00ea855a4117841a8f04e2deafca67e27b2
jdeuwyyKO6 <76080981+jdeuwyyKO6@users.noreply.github.com>
2020-12-16 02:33:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to Heroku https://github.com/jdeuwyyKO6/shadowsocks-heroku");
   });
 
   wss = new WebSocketServer({
